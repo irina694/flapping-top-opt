7d6d1fe772080e4acda4d3588ced7a7f3342bea8
Irina Patrikeeva <patrikeeva.irina@gmail.com>
2011-06-13 16:53:34 -1000

    removed global variables from callGA.m

---
+++ modified/config.m
@@ -0,0 +1,12 @@
+function Gvars = config(Gvars)
+
+Gvars.Nh = 20;
+Gvars.Nletters = 20;
+Gvars.NLetterperRule = 8;
+Gvars.Ncodes = 9;
+Gvars.NinitEdges = 4;
+Gvars.Nlevels = 4;
+% fraction of the tip with shell
+Gvars.nB = 1;
+
+end
---
+++ modified/userInput.m
@@ -0,0 +1,11 @@
+function input = userInput(input, Gvars)
+
+input.Nlevels = Gvars.Nlevels;
+input.Nletters = Gvars.Nletters;
+input.Ncodes = Gvars.Ncodes;
+input.NLetterperRule = Gvars.NLetterperRule;
+
+input.Nvertices = 4;
+input.vertCoord = [-1 -1; 1 -1; 1 1; -1 1];
+
+end
